c9f32706f2c54a8d6e12f994e9bf7a6360ada7c4
rakiburrahamanCS <mail.rakiburrahaman@gmail.com>
2022-10-17 19:15:23 +0600

    name.py up2

--- name.py
+++ name.py
@@ -1,4 +1,4 @@
 name = input("whats your name ?\n")
-age = int(input("what is your age ?"))
+ages = int(input("what is your age ?"))
 
-print(f"hi there {name}.good to know that you are {age}")
+print(f"hi there {name}.good to know that you are {ages}")
