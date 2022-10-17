29c66c2df247fee97ff3369570d22fe9d12a8b01
rakiburrahamanCS <mail.rakiburrahaman@gmail.com>
2022-10-17 20:08:11 +0600

    conditons.py up0

---
+++ conditions.py
@@ -0,0 +1,9 @@
+n = int(input("what is the number ?"))
+
+if n > 0:
+    print(f"{n} is a positive number.")
+
+elif n < 0:
+    print(f"{n} is a negative number.")
+else:
+    print(f"{n} is zero")
